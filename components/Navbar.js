b287f26b5f85f90fc95e788bebecbdad9765f5d9
Sn0w <clement.ozor@epitech.eu>
2022-05-28 19:22:17 +0200

    ✨ Add desktop navbar style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
+const links = [
+    { href: "/", label: "Home", active: "home" },
+    { href: "/about", label: "About", active: "about" },
+    { href: "https://docs.nextcitizens.net", label: "Documentation", active: "documentation" },
+    { href: "https://discord.gg/BgscMfAtJj", label: "Discord", active: "discord" },
+]
+
 const Navbar = ({active}) => {
+    const linksComponents = links.map(link => {
+        return (
+            <Link href={link.href} key={link.label}>
+                <a className={active === link.active ? "active" : ""}>{link.label}</a>
+            </Link>
+        )
+    })
+
     return (
         <nav className="navbar">
-
+            <div></div>
+            <div id={"links-container"}>
+                { linksComponents }
+            </div>
+            <div></div>
         </nav>
     )
 }
