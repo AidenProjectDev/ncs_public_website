7ee1f8c3791bc431064063a312196aa8e19f3911
Sn0w <clement.ozor@epitech.eu>
2022-05-28 19:48:58 +0200

    ✨ Add icon if the link needs to be open in new tab

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,19 @@
 import Link from "next/link";
+import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const links = [
-    { href: "/", label: "Home", active: "home" },
-    { href: "/about", label: "About", active: "about" },
-    { href: "https://docs.nextcitizens.net", label: "Documentation", active: "documentation" },
-    { href: "https://discord.gg/BgscMfAtJj", label: "Discord", active: "discord" },
+    { href: "/", label: "Home", active: "home", newTab: false},
+    { href: "/about", label: "About", active: "about", newTab: false},
+    { href: "https://docs.nextcitizens.net", label: "Documentation", active: "documentation", newTab: false },
+    { href: "https://discord.gg/BgscMfAtJj", label: "Discord", active: "discord", newTab: true },
 ]
 
 const Navbar = ({active}) => {
     const linksComponents = links.map(link => {
         return (
             <Link href={link.href} key={link.label}>
-                <a className={active === link.active ? "active" : ""}>{link.label}</a>
+                <a className={active === link.active ? "active" : ""} target={link.newTab ? "_blank" : ""}>{link.label} {link.newTab ? <FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{marginLeft: ".5rem", height: "16px"}}/> : ""}</a>
             </Link>
         )
     })
