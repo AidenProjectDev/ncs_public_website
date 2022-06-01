e1bfd1ceeb6820859907c80516a47e08d5a9a704
Sn0w <clement.ozor@protonmail.com>
2022-06-01 20:03:05 +0200

    🔧 Update NextJS config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
+
+module.exports = withTM({nextConfig});
